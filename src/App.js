bbe04562ea960e6e39852d4ecf4bd91bc55826f4
jeseias domingos <domingosjeseias@gmail.com>
2019-12-19 07:32:10 +0100

    Finished doing my routes everything is ready for start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Pages
+import Pages from './Pages/routes'
 
-export default App;
+// Styles
+import GlobalStyles from './Styles/GlobalStyles'
+
+export default () => 
+  <BrowserRouter>
+    <Pages />
+    <GlobalStyles /> 
+  </BrowserRouter>
