0ef88cffc7d540c16d640602cc5aa9d6ee4aff48
jeseias domingos <domingosjeseias@gmail.com>
2019-12-20 13:14:18 +0100

    Did some changes to the profile-page

--- src/Pages/Profile/index.js
+++ src/Pages/Profile/index.js
@@ -1,7 +1,25 @@
 import React from 'react' 
 
+import { MainTitle } from '../../Styles/typografy'
+import { ContentBox, FormInput, FormArea  } from '../../Styles/components'
 import { Container } from './styles'
 
 export default () =>
-  <Container> 
+  <Container className="PageContent"> 
+    <form>
+      <MainTitle> Sobre <div id="infinity"></div> </MainTitle>
+      <ContentBox className="me"> 
+        <div className="img">
+            <img alt=""/>
+        </div> 
+        <div className="details">
+          <h3>Jeseías Domingos</h3>
+          <p className="location"> 
+            <FormInput name="location" placeholder="location" type="text"/>
+          </p>
+          <FormArea name="info1" placeholder="Information one" />
+          <FormArea name="info2" placeholder="Information two" /> 
+        </div>
+      </ContentBox>
+    </form>
   </Container>
