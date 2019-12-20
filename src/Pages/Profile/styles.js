5603b7f1dd80c4059a21df5fce2f5674e8ef21fb
jeseias domingos <domingosjeseias@gmail.com>
2019-12-20 14:23:37 +0100

    Started working on my profile-page

--- src/Pages/Profile/styles.js
+++ src/Pages/Profile/styles.js
@@ -5,4 +5,9 @@ import { PageContainer } from '../../Styles/mixins'
 export const Container = styled.div`
   ${PageContainer};
   overflow-y: scroll;
+
+  .me {
+    display: grid;
+    grid-template-columns: 20% auto;
+  }
 `;
