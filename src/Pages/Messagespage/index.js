c34f9921c48d4d867558f6d21799f1d83d8cf457
jeseias domingos <domingosjeseias@gmail.com>
2020-01-01 12:32:59 +0100

    Getting one message is now working perfect

--- src/Pages/Messagespage/index.js
+++ src/Pages/Messagespage/index.js
@@ -1,8 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { Container } from './styles';
+import api from '../../services/api'
 
-export default () => 
-  <Container className="PageContent">
-    <h1>Message page</h1>
-  </Container>
+import Modal from '../../components/Modal'
+
+import { MainTitle } from '../../Styles/typografy' 
+import { Container, MessageContainer } from './styles';
+
+export default () => {
+  const [messages, setMessages] = useState([])
+  const [info, setModalInfo] = useState({})
+  const [display, setDisplay] = useState(false)
+
+  useEffect(() => { 
+    async function loadMessages() {
+      const data = await api.get('/msg')
+      setMessages(data.data)
+    }
+    loadMessages()
+  }, [])
+
+  const handleMessage = async id => {
+    setDisplay(!display) 
+    const data = await api.get(`/msg/${id}`) 
+    setModalInfo(data.data)
+  }
+
+  const toggleModal = () => setDisplay(!display)
+
+  return ( 
+    <Container className="PageContent" total={messages.length}>
+      <MainTitle> Mensagens <div id="infinity"></div> </MainTitle>
+      <Modal info={info} show={display} toggleModal={toggleModal}/>
+      <div className="messagebox">
+        {
+          messages.map( msg => 
+            <MessageContainer key={msg._id}> 
+              <h2 className="name">{msg.name}</h2>
+              <div className="date" onClick={() => handleMessage(msg._id)}>ler Mais</div>
+              <div className="msg">
+                {msg.msg.split('').map( (l, i) => {
+                    const saying = []
+                    if (i < 80) saying.push(l)  
+                    return `${saying.toString()}` 
+                })}...
+              </div>
+            </MessageContainer>
+          )
+        }
+      </div>
+    </Container>
+  )
+}
--- src/Pages/Messagespage/styles.js
+++ src/Pages/Messagespage/styles.js
@@ -1,7 +1,64 @@
 import styled from 'styled-components';
 
-import { PageContainer } from '../../Styles/mixins'
+import { PageContainer, smoothTransition } from '../../Styles/mixins'
+import { backgrounColor, tertiaryColor, secondaryColor, primaryColor } from '../../Styles/variables'
 
 export const Container = styled.div`
   ${PageContainer};
+
+  .messagebox {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: ${props => `repeat(${props.total/3}, 150px)`}; 
+    grid-gap: 1rem;
+    padding: 1rem 0;
+    width: 100%;
+    height: 100%;
+  }
 `;
+
+export const MessageContainer = styled.div`
+  background: ${backgrounColor};
+  color: #fff;
+  border-radius: 1rem;
+  box-shadow: 0 0 1rem rgba(0,0,0,.5);
+  ${smoothTransition};
+  padding: 1.5rem;
+  display: grid;
+  grid-template-areas: 
+    "name name name name"
+    "msg  msg msg msg" 
+    "msg  msg msg msg" 
+    ". . date date" ;
+
+  .name {
+    grid-area: name;
+    text-align: left;
+    color: ${tertiaryColor};
+    padding: 1rem 0;
+  }
+
+  .msg {
+    grid-area: msg;
+  }
+
+  .date {
+    grid-area: date;
+    background: ${secondaryColor};
+    font-weight: bold;
+    padding: .3rem .7rem;
+    border-radius: 3px;
+    cursor: pointer;
+    box-shadow: 0 0 .5rem #000;
+    ${smoothTransition};
+
+    &:hover {
+      background: ${tertiaryColor}; 
+    }
+  }
+
+  &:hover {
+    cursor: pointer;
+    transform: translateY(-10px);
+  }
+`
