e9b878bad82689fb8e3bb965ba1f1c87afc72f55
jeseias domingos <domingosjeseias@gmail.com>
2020-01-01 12:33:17 +0100

    The modal is now perfect

---
+++ src/components/Modal/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { MdClose } from 'react-icons/md'
+
+import { SecondTitle } from '../../Styles/typografy'
+import { Container } from './styles';
+
+export default ({ info: { name, msg }, show, toggleModal}) =>
+  <Container show={show}> 
+    <main>
+      <MdClose className="close" size={30} onClick={() => toggleModal()} /> 
+      <SecondTitle>{ name }</SecondTitle> 
+      <p className="text">
+        {msg}
+      </p>
+    </main>
+  </Container>
